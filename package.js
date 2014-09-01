1aeb52abb02400f96090ce9ee19485f890b6c739
Kelly <copleykj@gmail.com>
2014-09-01 15:00:31 -0400

    ready package for publishing to meteor packaging servers

--- package.js
+++ package.js
@@ -1,8 +1,16 @@
 Package.describe({
-    summary: "Beautifully Simple Reactive Publications"
+	name: "copleykj:simple-publish",
+
+    summary: "Beautifully Simple Reactive Publications",
+
+    version: "1.0.1",
+
+    git: "https://github.com/copleykj/meteor-simple-publish"
 });
 
 Package.on_use(function (api) {
+	api.versionsFrom('0.9.0');
+
     api.use('underscore', 'server');
 
     api.add_files('simple-publish.js', 'server');
